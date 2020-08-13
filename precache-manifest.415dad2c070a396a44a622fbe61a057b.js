9a76ac6b4a82b3083479fa0b5252f2d7aca58522
rohan23 <rohan@socialpilot.co>
2020-08-13 22:50:32 +0530

    Updates

---
+++ precache-manifest.415dad2c070a396a44a622fbe61a057b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "242f0fc15e8c256277a5064b32d5c682",
+    "url": "/rohanvachheta/google-keep-clone/index.html"
+  },
+  {
+    "revision": "fa05a6a8244bb5953476",
+    "url": "/rohanvachheta/google-keep-clone/static/css/2.f755b4bb.chunk.css"
+  },
+  {
+    "revision": "19ca25ccb1dd1cd131dd",
+    "url": "/rohanvachheta/google-keep-clone/static/css/main.08c68624.chunk.css"
+  },
+  {
+    "revision": "fa05a6a8244bb5953476",
+    "url": "/rohanvachheta/google-keep-clone/static/js/2.35a78f7d.chunk.js"
+  },
+  {
+    "revision": "b23a8a042e51dd51bbd0294055a0b702",
+    "url": "/rohanvachheta/google-keep-clone/static/js/2.35a78f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19ca25ccb1dd1cd131dd",
+    "url": "/rohanvachheta/google-keep-clone/static/js/main.5e622d2e.chunk.js"
+  },
+  {
+    "revision": "b721563c348fdd274fec",
+    "url": "/rohanvachheta/google-keep-clone/static/js/runtime-main.9073a070.js"
+  }
+]);
